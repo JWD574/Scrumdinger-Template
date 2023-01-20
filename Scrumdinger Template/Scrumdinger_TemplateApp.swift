ada1f04b39fd7c3b3c9f5912faec30eb1bab586c
rangerboy574 <jdeweese574@gmail.com>
2023-01-20 15:51:02 -0600

    Initial Commit

---
+++ Scrumdinger Template/Scrumdinger_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrumdinger_TemplateApp.swift
+//  Scrumdinger Template
+//
+//  Created by Joseph  DeWeese on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Scrumdinger_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
